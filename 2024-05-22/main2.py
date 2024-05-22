2497b6015a55916266081fa81caa193ade74d9e3
Pochang Lee <pochanglee2008@gmail.com>
2024-05-22 14:17:38 +0800

    autocommit

--- 2024-05-22/main2.py
+++ 2024-05-22/main2.py
@@ -7,12 +7,20 @@ app = Dash()
 
 app.layout = [
     # 試著加入2個文字輸入框, 使用 dcc.Input
-    dcc.Input(placeholder="請輸入文字"),
+    dcc.Input(placeholder="請輸入文字", id="text_input"),
     dcc.Input(placeholder="請輸入數字", type="number"),
 
     # 在頁面上顯示輸入的文字跟數字
     html.P('您輸入的文字是: '),
     html.P('您輸入的數字是: '),
 ]
+
+@app.callback(
+    Output(),
+    Input(),
+)
+def on_text():
+
+    return f'您輸入的文字是: ${text}'
 
 app.run(debug=True)
