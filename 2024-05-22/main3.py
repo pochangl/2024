17ba84380d46d1d94c1e1546f7e38bb4c014d626
Pochang Lee <pochanglee2008@gmail.com>
2024-05-22 16:16:02 +0800

    autocommit

--- 2024-05-22/main3.py
+++ 2024-05-22/main3.py
@@ -1,7 +1,12 @@
 # 目標建立 Hello world 的 dash 程式
 # 在網頁上顯示 Hello World
 
-from dash import Dash, dcc, html, Input, Output
+from dash import Dash, dcc, html, Input, Output, Graph
+import numpy as np
+import plotly.express as px
+
+x = np.arange(-5, 5)
+y = 2 * x + 3
 
 app = Dash()
 
@@ -13,6 +18,7 @@ app.layout = [
     # 在頁面上顯示輸入的文字跟數字
     html.P(children='您輸入的a是: ', id='a_output'),
     html.P(children='您輸入的b是: ', id='b_output'),
+    dcc.Graph(figure=px.line(x=x, y=y)),
 ]
 
 app.run(debug=True)
