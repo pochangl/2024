7e761502d8e3c0d46984314efe3ad403da8b2408
Pochang Lee <pochanglee2008@gmail.com>
2024-05-22 16:10:41 +0800

    autocommit

---
+++ 2024-05-22/main3.py
@@ -0,0 +1,35 @@
+# 目標建立 Hello world 的 dash 程式
+# 在網頁上顯示 Hello World
+
+from dash import Dash, dcc, html, Input, Output
+
+app = Dash()
+
+app.layout = [
+    # 試著加入2個文字輸入框, 使用 dcc.Input
+    dcc.Input(placeholder="請輸入 a", id="a_input", type="number", value=0),
+    dcc.Input(placeholder="請輸入 b", id="b_input", type="number", value=0),
+
+    # 在頁面上顯示輸入的文字跟數字
+    html.P(children='您輸入的文字是: ', id='text_output'),
+    html.P(children='您輸入的數字是: ', id='number_output'),
+]
+
+@app.callback(
+    Output(component_id='text_output', component_property='children'),
+    Input(component_id='text_input', component_property='value'),
+)
+def on_text(text):
+
+    return f'您輸入的文字是: {text}'
+
+## 數字欄位的 callback
+@app.callback(
+    Output(component_id='number_output', component_property='children'),
+    Input(component_id='number_input', component_property='value'),
+)
+def on_number(number):
+
+    return f'您輸入的數字是: {number}'
+
+app.run(debug=True)
