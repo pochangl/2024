5b1860c0d7fefa00ff8bd06d34b1ff18af01c2b9
Pochang Lee <pochanglee2008@gmail.com>
2024-05-22 13:42:53 +0800

    autocommit

--- 2024-05-22/main.py
+++ 2024-05-22/main.py
@@ -1,10 +1,10 @@
 # 目標建立 Hello world 的 dash 程式
 # 在網頁上顯示 Hello World
 
-from dash import Dash
+from dash import Dash, html
 
 app = Dash()
 
-app.layout = []
+app.layout = [html.P(children='Hello world')]
 
 app.run(debug=True)
