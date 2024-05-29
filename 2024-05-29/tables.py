27454160ec45de93cee3bfb999d7e1adb1af57e2
Charlie Lee <pochanglee2008@gmail.com>
2024-05-29 14:30:49 +0800

    autocommit

--- 2024-05-29/tables.py
+++ 2024-05-29/tables.py
@@ -33,9 +33,8 @@ app.layout = [
 def update(last_name):
     ## https://raw.githubusercontent.com/pochangl/2024/main/2024-05-08/customers.csv
     all_customers = pd.read_csv('customers.csv')[['customer_id', 'first_name', 'last_name']]
-    print(all_customers['last_name'] == last_name)
-    customers = all_customers[all_customers['last_name'] == last_name]
     print(last_name)
+    customers = all_customers[all_customers['last_name'] == last_name]
 
     ## https://raw.githubusercontent.com/pochangl/2024/main/2024-05-08/orders.csv
     orders = pd.read_csv('orders.csv')[['order_id', 'customer_id', 'order_date']]
