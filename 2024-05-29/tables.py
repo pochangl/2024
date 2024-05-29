e1ad65df0f7686ff32b018d2bccf69c4eef20362
Charlie Lee <pochanglee2008@gmail.com>
2024-05-29 13:43:50 +0800

    autocommit

--- 2024-05-29/tables.py
+++ 2024-05-29/tables.py
@@ -1,6 +1,11 @@
 from dash import Dash, html
 
+import pandas as pd
+
 app = Dash()
+
+## https://raw.githubusercontent.com/pochangl/2024/main/2024-05-08/customers.csv
+data = pd.read_csv('customers.csv')
 
 app.layout = [
     html.Div(children='Hello world')
