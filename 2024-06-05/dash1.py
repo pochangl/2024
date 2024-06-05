288792e257b3c8f0b3244b8e94c9518a204d5288
Charlie Lee <pochanglee2008@gmail.com>
2024-06-05 14:00:49 +0800

    autocommit

--- 2024-06-05/dash1.py
+++ 2024-06-05/dash1.py
@@ -1,4 +1,4 @@
-from dash import Dash, html, dcc, dash_table
+from dash import Dash, html, dcc, dash_table, Input, Output
 import pandas as pd
 
 customers = pd.read_csv('customers.csv')
@@ -6,11 +6,16 @@ customers = pd.read_csv('customers.csv')
 app = Dash()
 app.layout = html.Div(
     children=[
-        dcc.Input(id='filter', placeholder='請輸入您想尋找的資料'),
+        dcc.Input(id='filter', placeholder='請輸入您想尋找的資料', value=''),
         dash_table.DataTable(id='datatable', data=customers.to_dict('records')),
     ]
 )
 
 # 把 read_csv 跟 datatable data 的部份放到 callback 裡
+@app.callback([
+    [
+        Input(component_id='filter', component_property='value'),
+    ]
+])
         
 app.run(debug=True)
