bbaa3067d9fad09bfb7444893f06c6da76d18d74
Charlie Lee <pochanglee2008@gmail.com>
2024-06-05 14:02:56 +0800

    autocommit

--- 2024-06-05/dash1.py
+++ 2024-06-05/dash1.py
@@ -1,13 +1,12 @@
 from dash import Dash, html, dcc, dash_table, Input, Output
 import pandas as pd
 
-customers = pd.read_csv('customers.csv')
 
 app = Dash()
 app.layout = html.Div(
     children=[
         dcc.Input(id='filter', placeholder='請輸入您想尋找的資料', value=''),
-        dash_table.DataTable(id='datatable', data=customers.to_dict('records')),
+        dash_table.DataTable(id='datatable', data=[]),
     ]
 )
 
@@ -19,5 +18,8 @@ app.layout = html.Div(
 [
     Input(component_id='filter', component_property='value'),
 ])
+def onFilter(filter):
+    customers = pd.read_csv('customers.csv')
+    return customers.to_dict('records')
         
 app.run(debug=True)
