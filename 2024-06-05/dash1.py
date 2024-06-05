0073f6d6892fcd58adf4c1c40cf87c380d673e26
Charlie Lee <pochanglee2008@gmail.com>
2024-06-05 13:45:20 +0800

    autocommit

--- 2024-06-05/dash1.py
+++ 2024-06-05/dash1.py
@@ -1,10 +1,10 @@
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, dash_table
 import pandas as pd
 
 customers = pd.read_csv('customers.csv')
 
 app = Dash()
-app.layout = dcc.DataTable()
+app.layout = dash_table.DataTable()
 
 
 app.run(debug=True)
