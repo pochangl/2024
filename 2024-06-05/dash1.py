bde2bc3321f3ba702afd97546bd6239d13d4c227
Charlie Lee <pochanglee2008@gmail.com>
2024-06-05 14:16:07 +0800

    autocommit

--- 2024-06-05/dash1.py
+++ 2024-06-05/dash1.py
@@ -24,7 +24,8 @@ def onFilter(text: str):
     # 把 last_name 抽出來, 然後每個比對看有沒有包含 text 的字串
     filter1 = customers['last_name'].str.contains(text)
     filter2 = customers['first_name'].str.contains(text)
-    customers = customers[filter1 | filter2]
+    filter3 = customers['customer_id'].str.contains(text)
+    customers = customers[filter1 | filter2 | filter3]
     data = customers.to_dict('records')
     return [data]
         
